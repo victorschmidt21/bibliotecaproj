4f27d7af3e01e9893fa832b03c0bcaf64e11024a
victorschmidt21 <163210321+victorschmidt21@users.noreply.github.com>
2024-04-07 20:52:34 -0300

    Add files via upload

---
+++ codigo base/variaveis.h
@@ -0,0 +1,36 @@
+#ifndef VARIAVEIS_H
+#define VARIAVEIS_H
+
+FILE* arquivo;
+#define tamanho 100
+#define arquivoFechar fclose(arquivo);
+#define arquivoLeitura arquivo = fopen(nomedoarquivo, "r");
+#define arquivoEditar arquivo = fopen(nomedoarquivo, "a");
+#define lerInformacoes array[elementoArray], tamanho, arquivo
+
+int elementoArray = 0;
+int tecla = 0;
+char array[tamanho][tamanho];
+
+struct informacoesLivros {
+    char nome[50];
+    int genero;
+    char autor[50];
+    int emprestimo;
+
+
+};
+
+struct informacoesClientes {
+    char nome2[50];
+    char cpf[12];
+    char telefone[11];
+    char dataDeNascimento[8];
+    int emprestimo;
+};
+
+struct informacoesClientes clientes[tamanho];
+
+struct informacoesLivros livros[tamanho];
+
+#endif
